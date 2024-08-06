4bed8757909621dcd76fd3c21f8ac3eef3af99cd
DELL <DELL@DESKTOP-7GDGRGH>
2024-08-06 14:34:53 +0530

    Get user datails and print on console

---
+++ FirstWebProject2/src/main/java/com/servlets/RegisterServlet.java
@@ -0,0 +1,53 @@
+package com.servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public RegisterServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("do post executed");
+		String name = request.getParameter("uname");
+		String number = request.getParameter("number");
+		String password = request.getParameter("password");
+		System.out.println(name +" " +number+" "+ password);
+		
+		if(name.equals("admin") && password.equals("admin")) {
+			System.out.println("login successfull");
+		}
+		else {
+			System.out.println("login unsuccess");
+		}
+		
+	}
+
+}
